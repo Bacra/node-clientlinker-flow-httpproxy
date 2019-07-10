e051a2843c873bb737a292ebb5d5ce55655bbf50
Bacra <bacra.woo@gmail.com>
2019-07-11 02:26:50 +0800

    Add: signature test

--- test/cert/sh.sh
+++ test/cert/sh.sh
@@ -4,4 +4,3 @@ openssl genrsa -out rsa_private_key.pem 1024
 openssl pkcs8 -topk8 -inform PEM -in rsa_private_key.pem -outform PEM –nocrypt
 # 生成对应公钥
 openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
-
--- test/cert/test.js
+++ test/cert/test.js
@@ -2,28 +2,45 @@ var fs = require('fs');
 var crypto = require('crypto');
 var constants = crypto.constants || require('constants');
 
-function rsaDecrypt(text)
-{
-	var textBuffer= new Buffer(text);
-	var decryptText= crypto.privateDecrypt(
-		{
-			key: fs.readFileSync(__dirname + '/rsa_private_key.pem'), // 如果通过文件方式读入就不必转成Buffer
-			padding: constants.RSA_PKCS1_PADDING // 因为前端加密库使用的RSA_PKCS1_PADDING标准填充,所以这里也要使用RSA_PKCS1_PADDING
-		}, textBuffer);
-
-	return decryptText;
-}
-
 function rsaEncrypt(text)
 {
 	var textBuffer= new Buffer(text);
 	var encryptText= crypto.publicEncrypt(
 		{
 			key: fs.readFileSync(__dirname + '/rsa_public_key.pem'), // 如果通过文件方式读入就不必转成Buffer
-			padding: constants.RSA_PKCS1_PADDING // 因为前端加密库使用的RSA_PKCS1_PADDING标准填充,所以这里也要使用RSA_PKCS1_PADDING
+			padding: constants.RSA_PKCS1_PADDING
 		}, textBuffer);
 
 	return encryptText;
+}
+
+function rsaDecrypt(text)
+{
+	var textBuffer= new Buffer(text);
+	var decryptText= crypto.privateDecrypt(
+		{
+			key: fs.readFileSync(__dirname + '/rsa_private_key.pem'), // 如果通过文件方式读入就不必转成Buffer
+			padding: constants.RSA_PKCS1_PADDING
+		}, textBuffer);
+
+	return decryptText;
+}
+
+function getSign(text)
+{
+	var sign = crypto.createSign('SHA256');
+	sign.update(text);
+	sign.end();
+
+	return sign.sign(fs.readFileSync(__dirname + '/rsa_private_key.pem'));
+}
+
+function verifySign(text, signature)
+{
+	var verify = crypto.createVerify('SHA256');
+	verify.update(text);
+	verify.end();
+	return verify.verify(fs.readFileSync(__dirname + '/rsa_public_key.pem'), signature);
 }
 
 var str = 'hello word';
@@ -32,3 +49,9 @@ console.log('str1', buf1.toString('base64'));
 
 var buf2 = rsaDecrypt(buf1);
 console.log('str2', buf2.toString());
+
+
+var signature = getSign(str);
+console.log('signature', signature.toString('base64'));
+
+console.log(verifySign(str, signature));
